70cc8af42e3a1723536407c3ee1c7e4810c0ac42
Oscar Carrasco <oscar.hcarrasco03@gmail.com>
2022-03-19 12:04:25 -0700

    modelo y migracion de categorias

---
+++ database/factories/Configs/CategoryFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories\Configs;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Configs\Category>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
