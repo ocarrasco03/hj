fef9bf9a35d3e8c6213195606344ea34b1186933
Oscar Carrasco <oscar.hcarrasco03@gmail.com>
2022-04-01 16:13:46 -0700

    cambios

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -18,7 +18,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('queue:work')->everyMinute();
+        // $schedule->command('queue:work')->everyMinute();
         // $schedule->command('import:products')->everyMinute();
     }
 
